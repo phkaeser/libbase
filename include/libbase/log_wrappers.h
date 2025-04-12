54c928ceeae547f39edaeb566277da294db17aa0
Philipp Kaeser <130065133+phkaeser@users.noreply.github.com>
2025-04-12 12:47:44 +0200

    Fixes a conflicting semicolon for logged_.* wrappers. (#39)

--- include/libbase/log_wrappers.h
+++ include/libbase/log_wrappers.h
@@ -60,7 +60,7 @@ static inline void *_logged_malloc(
 
 /** Calls malloc(3) and logs error with ERROR severity. */
 #define logged_malloc(size)                     \
-    _logged_malloc(__FILE__, __LINE__, size);
+    _logged_malloc(__FILE__, __LINE__, size)
 
 /** Helper: Calls strdup(3) and logs errors for given file & line. */
 static inline char *_logged_strdup(
@@ -76,7 +76,7 @@ static inline char *_logged_strdup(
 
 /** Calls strdup(3) and logs error with ERROR severity. */
 #define logged_strdup(str)                      \
-    _logged_strdup(__FILE__, __LINE__, str);
+    _logged_strdup(__FILE__, __LINE__, str)
 
 
 #ifdef __cplusplus
