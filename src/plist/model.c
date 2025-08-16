68c8a912e60786c05ee7fe96a7bb611c538e18c7
Philipp Kaeser <kaeser@gubbe.ch>
2025-08-16 10:23:35 +0200

    libbase: Add tests for bspl_string_value_from_object.

--- src/plist/model.c
+++ src/plist/model.c
@@ -516,6 +516,16 @@ void test_string(bs_test_t *test_ptr)
         BSPL_STRING,
         bspl_object_type(object_ptr));
 
+    BS_TEST_VERIFY_STREQ(
+        test_ptr,
+        "a test",
+        bspl_string_value_from_object(object_ptr));
+
+    BS_TEST_VERIFY_EQ(test_ptr, NULL, bspl_string_value_from_object(NULL));
+    bspl_object_unref(object_ptr);
+
+    object_ptr = bspl_object_from_array(bspl_array_create());
+    BS_TEST_VERIFY_EQ(test_ptr, NULL, bspl_string_value_from_object(NULL));
     bspl_object_unref(object_ptr);
 }
 
